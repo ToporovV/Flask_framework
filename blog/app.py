86758e50fc6faa301a904654fb1e5ab6fb2b541d
toporovv <top.vetal@yandex.ru>
2021-05-09 21:46:08 +0300

    homework 2

---
+++ blog/app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from blog.articles.views import article
+from blog.user.views import user
+
+
+def create_app() -> Flask:
+    app = Flask(__name__)
+    register_blueprints(app)
+    return app
+
+
+def register_blueprints(app: Flask):
+    app.register_blueprint(user)
+    app.register_blueprint(article)
